abda9246d57d984da7adeab4eb5d32e354083507
Laxman Thota <laxmant.thota@gmail.com>
2020-08-30 18:10:48 +0530

    updated code

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, { "Content-Type": "text/html" });
-    response.end("<html><body><h1>Hello World!</h1></body></html>");
+    response.end("<html><body><h1>Hello World welcome to Azure world apps!</h1></body></html>");
 
 });
-
+ 
 var port = process.env.PORT || 1337;
 server.listen(port);
 
